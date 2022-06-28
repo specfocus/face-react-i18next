a51dbcb4b088b8bbd60d19e84e60992c1d9d3197
Lucas Oromi <lucas.oromi@kyndryl.com>
2022-06-28 16:32:32 -0600

    fix provide value

--- translations/provideTranslationContextValue.ts
+++ translations/provideTranslationContextValue.ts
@@ -1,7 +1,7 @@
 import type { Locale, LocaleResources } from '@specfocus/view-focus.i18n/translations/Locale';
 import type { TOptions } from '@specfocus/view-focus.i18n/translations/Translation';
-import type { TranslationProvider } from '@specfocus/view-focus.i18n/translations/TranslationProvider';
-import i18n from 'i18next';
+import type { TranslationContextValue } from '@specfocus/view-focus.i18n/translations/TranslationContext';
+import i18n, { TFunction } from 'i18next';
 import { useTranslation as useI18nextTranslation } from 'react-i18next';
 
 // https://jaysoo.ca/2014/03/20/i18n-with-es2015-template-literals/
@@ -25,34 +25,36 @@ export type ResourcesPool = (
  *     fr: french,
  *     en: english,
  * };
- * const TranslationProvider = createTranslationProvider(locale => messages[locale])
+ * const TranslationContextValue = createTranslationProvider(locale => messages[locale])
  */
-export const createTranslationProvider = (
+export const provideTranslationContextValue = (
   pullResources: ResourcesPool,
   fallbackLocale: Locale = 'en'
-): TranslationProvider => {
+): TranslationContextValue => {
   let _locale = fallbackLocale;
+  let _t: TFunction = (key: string) => key;
   const messages = pullResources(fallbackLocale);
   if (messages instanceof Promise) {
     throw new Error(
       `The pullResources returned a Promise for the messages of the default locale (${fallbackLocale}). Please update your pullResources to return the messages of the default locale in a synchronous way.`
     );
   }
- const { t } = useI18nextTranslation();
   return {
-    translate: (key: string, options: TOptions = {}) => t(key, options),
+    translate: (key: string, options: TOptions = {}) => _t(key, options),
     changeLocale: (locale: string) =>
       // We systematically return a Promise for the messages because
       // pullResources may return a Promise
       Promise.resolve(pullResources(locale)).then(
         (resources: LocaleResources) => {
-          _locale = locale;
           i18n.init({
             fallbackLng: fallbackLocale,
             lng: locale,
             resources: {
               [locale]: resources
             }
+          }).then(t => {
+            _locale = locale;
+            _t = t;
           });
         }
       ),
