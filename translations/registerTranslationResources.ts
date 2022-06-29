f53211313f82545c1c1352eafb9b746fe9f15688
Lucas Oromi <lucas.oromi@kyndryl.com>
2022-06-29 08:15:15 -0600

    update

--- translations/registerTranslationResources.ts
+++ translations/registerTranslationResources.ts
@@ -2,6 +2,8 @@ import i18n, { type InitOptions } from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import I18NextHttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
+// import intervalPlural from 'i18next-intervalplural-postprocessor';
+import Polyglot from 'i18next-polyglot';
 
 export interface RegisterTranslationOptions extends InitOptions {
   browser?: boolean;
@@ -15,14 +17,16 @@ export const registerTranslationResources = ({
   browser,
   ...options
 }: RegisterTranslationOptions = {
-  browser: true,
-  defaultNS: 'general',
-  fallbackLng: 'en',
-  ns: ['general']
-}) =>
+    browser: true,
+    defaultNS: 'general',
+    fallbackLng: 'en',
+    ns: ['general']
+  }) =>
   i18n
     .use(I18NextHttpBackend)
     .use(LanguageDetector)
+    .use(Polyglot)
+    // .use(intervalPlural) // https://www.i18next.com/translation-function/plurals#interval-plurals
     .use(initReactI18next) // passes i18n down to react-i18next
     .init({
       ...options
